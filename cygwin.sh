bd735553ae6651e1e910a1603cb688be192624f0
Greg Merrill <greghmerrill@gmail.com>
2011-08-01 18:08:20 -0500

    formatting nit

--- cygwin.sh
+++ cygwin.sh
@@ -5,7 +5,7 @@ export DT="$(cygpath -ua $USERPROFILE)/Desktop"
 # Export convenient variables for drives like C, D, etc
 for drive in $(find /cygdrive -mindepth 1 -maxdepth 1 -type d)
 do
-	export $(echo ${drive##*/} | tr [:lower:] [:upper:])=$drive
+  export $(echo ${drive##*/} | tr [:lower:] [:upper:])=$drive
 done
 
 # Open an Editor on the given file(s)
