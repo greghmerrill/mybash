72b0b7dfa943b41f4f3721d286d1381ecfc5c707
Greg Merrill <greghmerrill@gmail.com>
2011-08-22 20:31:50 -0500

    add current dir to path, small case correction in NPP_PY

--- cygwin.sh
+++ cygwin.sh
@@ -2,7 +2,7 @@
 
 export DT="$(cygpath -ua $USERPROFILE)/Desktop"
 export NPP="/cygdrive/c/Program Files (x86)/Notepad++/notepad++.exe"
-export NPP_PY="$(cygpath -ua $APPDATA)/Notepad++/plugins/Config/PythonScript/scripts"
+export NPP_PY="$(cygpath -ua $APPDATA)/Notepad++/plugins/config/PythonScript/scripts"
 
 # Export convenient variables for drives like C, D, etc
 for drive in $(find /cygdrive -mindepth 1 -maxdepth 1 -type d)
